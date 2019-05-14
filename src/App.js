031475c296aa389f44bb57a2cd6c2a7f82cd06fd
Lakshmi1970 <lakshmicrtvm@gmail.com>
2019-05-14 22:17:54 +0530

    code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          My App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hai</p>
     </div>
   );
 }
